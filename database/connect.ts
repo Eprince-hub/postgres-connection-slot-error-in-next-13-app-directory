7269b4b9befc3d629d3144ceadba287c602057df
Victor Ejike Nwosu <ejikello123@yahoo.com>
2023-02-01 12:38:54 +0100

    Format code

--- database/connect.ts
+++ database/connect.ts
@@ -1,22 +1,23 @@
-import { config } from 'dotenv-safe';
 import postgres from 'postgres';
 
-config();
+const sql = postgres();
 
-const sql = postgres({});
+type Fruit = {
+  id: number;
+  name: string;
+  type: string;
+};
 
 export async function getFruits() {
-  const fruits = await sql<
-    { id: number; name: string; type: string }[]
-  >`SELECT * FROM fruits;`;
+  const fruits = await sql<Fruit[]>`
+    SELECT * FROM fruits;
+  `;
   return fruits;
 }
 
 console.log(
   'Connection Count: ',
-
   sql`
-select count(*) from pg_stat_activity;
-
+    select count(*) from pg_stat_activity;
 `.then((x) => console.log('x: ', x)),
 );
